1d5ee0b9f7cf1f7ae950e5426d2bd9d8928c983c
guojiayu <ifishcn@gmail.com>
2014-08-08 21:59:50 +0800

    Initial commit

---
+++ app/src/main/java/com/fanr/core/IntentFactory.java
@@ -0,0 +1,5 @@
+package com.fanr.core;
+
+public class IntentFactory {
+    //TODO implement an Activity and Fragment delegate pattern
+}
---
+++ app/src/main/java/com/fanr/core/PauseTimerEvent.java
@@ -0,0 +1,7 @@
+package com.fanr.core;
+
+/**
+ * Marker class for Otto for a pause event for the timer.
+ */
+public class PauseTimerEvent {
+}
